9a86481d9a38f07519f8a2931f89eefc1b73372d
Deepak Pandey <pandeydeepakcse123@gmail.com>
2026-09-21 17:09:11 +0530

    feat: pre-configure production DB credentials in wp-config

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,14 @@ define('WP_CACHE', false); // Disabled for local development
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'igvk_marketing_wp' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+if ( file_exists( __DIR__ . '/wp-config-local.php' ) ) {
+    require_once __DIR__ . '/wp-config-local.php';
+} else {
+    define( 'DB_NAME', 'earningin_instagramvk' );
+    define( 'DB_USER', 'earningin_instagramvk' );
+    define( 'DB_PASSWORD', '4-^rR-XAGS0_mb1T' );
+    define( 'DB_HOST', 'localhost' );
+}
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
